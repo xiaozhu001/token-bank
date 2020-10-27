99226036b764c818ac791f92540a08b3695dfbe1
xuanbing <m15832013094@163.com>
2020-10-27 21:01:39 +0800

    abc

--- contracts/ITokenBank.sol
+++ contracts/ITokenBank.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.5.0 <= 0.7.0;
 
 interface ITokenBank {
-	function removeToken(string memory shorthandName) external;
+	function removeToken(string calldata shorthandName) external;
 }
